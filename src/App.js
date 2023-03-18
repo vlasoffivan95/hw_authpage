d895c7653495fd9263f049a1a97c6f0c505087e8
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-03-18 22:32:14 +0200

    add navigation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Footer from "./components/Footer";
 import LoginUser from "./components/LoginUser";
 import SignUp from "./components/SignUp";
 import NotFound from "./components/NotFound404";
-
-
-
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <main className={styles.appContainer}>
       <Header />
-      {/* <LoginUser/> */}
-      {/* <SignUp/> */}
-      <NotFound/>
+      <Switch>
+        <Route path="/" component = {LoginUser} exact />
+        <Route path="/login" component={LoginUser} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="*" component={NotFound} />
+      </Switch>
       <Footer />
     </main>
   );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styles from "./header.module.scss";
-import logo from './logo.webp'
+import logo from "./logo.webp";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
-const Header = () => {
+
+
+
+function Header() {
+  const location = useLocation();
   return (
     <header>
       <div className={styles.header}>
-        <img src={logo} alt='logo'/>
-        <button className = {styles.btn}>Sign Up</button>
+        <img src={logo} alt="logo" />
+        <button className={styles.btn}>
+          <Link to={location.pathname === "/signup" ? "/login" : "/signup"} className = {styles.link}>
+            {location.pathname === '/signup' ? "Login" : "SignUp"}
+          </Link>
+        </button>
       </div>
     </header>
   );
-};
+}
 
 export default Header;
